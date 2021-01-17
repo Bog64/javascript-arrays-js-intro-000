89ceff2c27f78cfeffe70362119f23d20df6e37b
Ben Gorenstein <bengorenstein@gmail.com>
2021-01-17 01:21:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element){
-  var newArray = array
-  newArray.unshift(element)
-  return newArray
+function addElementToBeginningOfArray(array, 'element'){
+  //var newArray = array
+  //newArray.unshift(element)
+  //return newArray
+  ['element',...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
